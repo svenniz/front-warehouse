994b44223a8173e9b7a9175ce14772bc4ee3f26a
Andreas Ventzel <a-spac01@specialisterne.dk>
2025-01-06 11:14:15 +0100

    HouseEdit + routes

--- ReactWeb/src/main/App.tsx
+++ ReactWeb/src/main/App.tsx
@@ -4,6 +4,8 @@ import Header from './Header'
 import HouseList from '../house/HouseList'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HouseDetails from '../house/HouseDetails'
+import HouseAdd from '../house/HouseAdd'
+import HouseEdit from '../house/HouseEdit'
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HouseList/>}></Route>
         <Route path="/house/:id" element={<HouseDetails/>}></Route>
+        <Route path="/house/add" element={<HouseAdd/>}></Route>
+        <Route path="/house/edit/:id" element={<HouseEdit/>}></Route>
       </Routes>
     </div>
     </BrowserRouter>
