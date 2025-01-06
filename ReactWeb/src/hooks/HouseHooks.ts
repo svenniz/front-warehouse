800e0d9e2aa23e5a2302c05afb590909e82cbe4d
Andreas Ventzel <a-spac01@specialisterne.dk>
2025-01-06 11:07:40 +0100

    HouseAdd and HouseForm

--- ReactWeb/src/hooks/HouseHooks.ts
+++ ReactWeb/src/hooks/HouseHooks.ts
@@ -1,7 +1,8 @@
 import { House } from "../types/house";
 import config from "../config";
-import { useQuery } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useNavigate } from "react-router-dom";
 
 const useFetchHouses = () => {
     return useQuery<House[], AxiosError>({
@@ -17,6 +18,46 @@ const useFetchHouse = (id:number) => {
             axios.get(`${config.baseApiUrl}/house/${id}`).then((res)=>res.data)
     });
 }
+// useMutation for POST/PUT/DELETE. Axios does the request
+const useAddHouse = () => {
+    const nav = useNavigate();
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError, House>({
+        mutationFn: (h) => axios.post(`${config.baseApiUrl}/houses`, h),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["houses"]
+            });
+            nav("/");
+        },
+    });
+};
+const useUpdateHouse = () => {
+    const nav = useNavigate();
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError, House>({
+        mutationFn: (h) => axios.put(`${config.baseApiUrl}/houses`, h),
+        onSuccess: (_, house) => {
+            queryClient.invalidateQueries({
+                queryKey: ["houses"]
+            });
+            nav(`/house/${house.id}`);
+        },
+    });
+};
+const useDeleteHouse = () => {
+    const nav = useNavigate();
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError, House>({
+        mutationFn: (h) => axios.delete(`${config.baseApiUrl}/houses/${h.id}`),
+        onSuccess: (_, house) => {
+            queryClient.invalidateQueries({
+                queryKey: ["houses"]
+            });
+            nav("/");
+        },
+    });
+};
 
 export default useFetchHouses;
-export  {useFetchHouse};
+export  {useFetchHouse, useAddHouse, useUpdateHouse, useDeleteHouse};
